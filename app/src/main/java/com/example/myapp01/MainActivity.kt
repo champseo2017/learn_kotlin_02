02afd93472e9be0f2786283cbb6c956c5b09c981
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-12-22 09:07:49 +0700

    Snackbar

--- app/src/main/java/com/example/myapp01/MainActivity.kt
+++ app/src/main/java/com/example/myapp01/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapp01
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity: AppCompatActivity() {
@@ -15,20 +16,8 @@ class MainActivity: AppCompatActivity() {
             tvOutput.text = "สวัสดี Android-Kotlin"
             Toast.makeText(this, "สวัสดี Android-Kotlin", Toast.LENGTH_LONG).show()
         }
+        fabShow.setOnClickListener {view ->
+            Snackbar.make(view, "Hello Snackbar", Snackbar.LENGTH_LONG).show()
+        }
     }
-    /*
-    *  เมธอด onCreate() ใน class MainActivity ถูกสั่งให้ทำงานเป็นลำดับแรกโดย
-    * อัตโนมัติ
-    * เมธอด setContentView() สั่งให้ใช้ส่วนแสดงผลที่ชื่อว่า activity_main.xml แสดง
-    * ออกมาเป็นหน้าจอแรก (setContentView(R.layout.activity_main))
-    *
-    *  เมื่อผู้ใช้งานคลิกปุ่ม cmdShow จะมีเหตุการณ์ setOnClickListener() ขึ้นมา
-    *  การทำงานภายในเหตุการณ์มี 2 อย่างคือ
-    *  1. กำหนดให้ TextView ที่ชื่อว่า tvOutput แสดงข้อความ "สวัสดี Android-Kotlin"
-    *  2. กำหนดให้แสดงข้อความแจ้งเตือนแบบ Toast ระยะเวลาปรากฏข้อความแบบ Toast สามารถกำหนดได้
-    *     2 แบบคือ ระยะเวลานาน (LENGTH_LONG) กับ ระยะสั้น (LENGTH_SHORT)
-    *  3. สามารถระบุ widget ต่างๆ ได้ ที่อยู่ในไฟล์ activity_main.xml ได้โดย code ของ
-    *     ภาษา Kotlin เกิดจาก Package ที่ชื่อว่า kotlinx.android.synthetic.main.activity_main.*
-    *
-    *  */
 }
