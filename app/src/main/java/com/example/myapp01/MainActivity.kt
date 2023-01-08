10413d9d19670e5561d103ee1ed80e732ce84a6f
ChampKos <32265715+champseo2017@users.noreply.github.com>
2023-01-08 11:49:07 +0700

    การกำหนดตำแหน่งกึ่งกลาง (แนวตั้งหรือแนวนอนของ ConstraintLayout)

--- app/src/main/java/com/example/myapp01/MainActivity.kt
+++ app/src/main/java/com/example/myapp01/MainActivity.kt
@@ -12,25 +12,19 @@ class MainActivity: AppCompatActivity() {
         /* ใน Subclass เราโอเวอร์ไรด์สมาชิกตัวใดไปแล้ว แต่ต้องการกลับไปใช้รูปแบบเดิมใน
    Superclass ให้อ้างถึงสมาชิกตัวนั้นด้วยคีย์เวิร์ด super  */
         setContentView(R.layout.activity_main)
-        cmdShow.setOnClickListener{ view ->
-            tvOutput.text = "สวัสดี Android-Kotlin"
-            Toast.makeText(this, "สวัสดี Android-Kotlin", Toast.LENGTH_LONG).show()
-        }
-        fabShow.setOnClickListener {view ->
-            Snackbar.make(view, "Hello Snackbar", Snackbar.LENGTH_LONG).show()
-        }
+//        cmdShow.setOnClickListener{ view ->
+//            tvOutput.text = "สวัสดี Android-Kotlin"
+//            Toast.makeText(this, "สวัสดี Android-Kotlin", Toast.LENGTH_LONG).show()
+//        }
+//        fabShow.setOnClickListener {view ->
+//            Snackbar.make(view, "Hello Snackbar", Snackbar.LENGTH_LONG).show()
+//        }
     }
-    /* การกำหนดขนาด widget
-       - มีวิธีการ 3 แบบที่ใช้
-         1. wrap_content มีขนาดเท่ากับเนื้อหาที่ตัวเองแสดงอยู่ wrap_content (android:layout_width="wrap_
-         content")
-         2. match_parent กำหนดให้ widget มีขนาดเท่ากับตัวบรรจุที่ตัวเองอาศัยอยู่ เช่น
-           widget TextView ตัวนี้อยู่ใน ConstraintLayout จะใช้พื้นที่เต็มส่วนแสดงผลทั้งความกว้าง
-           และ ความยาว ถ้ากำหนด ให้ TextView มีความกว้างแบบ match_parent จะทำให้ TextView
-           ใช้่ความกว้างของ ConstraintLayout ที่ตัวเองอาศัยอยู่
-
-          3. แบบกำหนดขนาดในหน่วย dp
-             - การกำหนดขนาดตายตัวในหน่วย dp เป็นการกำหนดขนาดเป็นตัวเลขตายตัว มีขนาดเป็นไปตามความละเอียดของ
-             หน้าจอที่กำลังแสดงผลอยู่ เช่น กำหนดความกว้าง 150dp (android:layout_width="150dp")
+    /*  การกำหนดตำแหน่งกึ่งกลาง (แนวตั้งหรือแนวนอนของ ConstraintLayout)
+       - การกำหนดแนวตั้งหรือแนวนอน 2 ครั้งกับ ConstraintLayout หมายถึง กำหนดให้ widget นั้นๆ
+        อยู่กึ่งกลาง
+        - ซ้ายและขวาในเวลาเดียวกัน คือ กำหนดตำแหน่งกึ่งกลางตามนแนวนอน
+        - บนและล่างในเวลาเดียวกัน คือ การกำหนดตำแหน่งกึ่งกลางตามแนวตั้ง
+        - การเทน้ำหนัก (app:layout_constraintHorizontal_bias="0.68")
     * */
 }
